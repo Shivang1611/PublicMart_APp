0e87407703b27be4b05a455d28105d8053d8ee44
Shivang1611 <shivangshukla306@gmail.com>
2025-04-27 09:49:09 +0530

    Adding Navgation

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+//for navigation
+import {NavigationContainer} from "@react-navigation/native"
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+//Screens
+import Home from './Screens/Home'
+import Product from './Screens/Product'
+import Details from './Screens/Details'
+import Categories from './Screens/Categories'
+
+
+//types
+export type Rootstackparamlist={
+  Home:{
+    home:Home
+  },
+  Categories:{
+    category:Product
+  },
+  Details:{
+    details:Product
+  },
+  Product:{
+    product:Product
+  }
+}
+
+
+const stack=createNativeStackNavigator<Rootstackparamlist>()
+ 
+
+const App = () => {
+  return (
+   <NavigationContainer>
+    <stack.Navigator initialRouteName='Home'>
+      <stack.Screen name="Home" component={Home}/>
+      <stack.Screen name="Categories" component={Categories} options={{
+        title:"Categories"
+      }}/>
+      <stack.Screen name="Product" component={Product} options={{
+        title:"Product"
+      }}/>
+      <stack.Screen name="Details" component={Details} options={{
+        title:"Details"
+      }}/>
+    </stack.Navigator>
+   </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({})
+export default App
+
---
+++ src/index.d.ts
@@ -0,0 +1,18 @@
+interface Product{
+    id:string,
+    name:string,
+    imageUrl:string,
+    originalPrice:number,
+    
+    discountPrice:number,
+    offerPercentage:number,
+    rating:number,
+    ratingCount:number,
+    tags: string[]
+
+}
+interface Home{
+    id:1,
+    name:string,
+   image:string
+}
